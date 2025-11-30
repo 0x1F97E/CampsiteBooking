fea0d9b2f6178cb2d4b3d7dddab52e631e09c2fd
BOOT <0x1f97E@gmail.com>
2025-11-30 16:26:00 +0100

    Refactor code structure for improved readability and maintainability

--- Tests/obj/Debug/net9.0/CampsiteBooking.Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net9.0/CampsiteBooking.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampsiteBooking.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83173e609a1d4a8473782c6e2ec8ca9dcce86aea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+504622b3a1be107692f6fc9bc3664b529e88b2ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampsiteBooking.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampsiteBooking.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
